d0ccc79469ddeece3439348b00264470f74dccfb
andriymarych <andriumarich@gmail.com>
2023-05-29 17:48:01 +0300

    Refactored login and registration pages

--- src/main/resources/static/js/common/login-form.js
+++ src/main/resources/static/js/common/login-form.js
@@ -1,8 +1,7 @@
 const loginBtn = document.getElementById("main-login-button");
 
 const closeLoginFormBtn = document.getElementById("close-login-button");
-
-const loginFormPopUpButton = document.getElementById("login-form-button");
+const loginLabel = document.getElementById("login-label");
 
 
 const userButton = document.getElementById("user-button");
@@ -13,7 +12,9 @@ const controlDisapearingBG = (flag) => {
     shadowBG.style.backgroundColor = "rgba(0, 0, 0, 0.6)";
     (flag)
         ? shadowBG.style.visibility = "visible"
-        : shadowBG.style.visibility = "hidden";
+        :
+        shadowBG.style.visibility = "hidden"
+        clearPopUp();
 }
 
 
@@ -31,78 +32,6 @@ shadowBG.addEventListener('click', () => {
     controlDisapearingBG(0);
 })
 
-loginFormPopUpButton.addEventListener('click', () => {
-    document.querySelector(".login-popup").classList.add("active");
-
-    controlDisapearingBG(1);
-});
-
-const resisterNewUser = () => {
-    const label = document.getElementById('registration-label');
-
-    const name = document.getElementById('registration-name').value;
-    const surname = document.getElementById('registration-surname').value;
-    const email = document.getElementById('registration-email').value;
-    const password = document.getElementById('registration-password').value;
-    const dubpassword = document.getElementById('repeat-password').value;
-    if (name === '' || surname === '' || email === '' || password === '' || dubpassword === '') {
-        label.innerHTML = '*Заповніть всі поля!';
-        return;
-    }
-    else {
-        label.innerHTML = '';
-    }
-    const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if(mailFormat.test(email)) {
-        label.innerHTML = '';
-    }
-    else {
-        label.innerHTML = '*Цей формат емейлу не відповідає нормам!';
-        return;
-    }
-
-    if (password !== dubpassword) {
-        label.innerHTML = '*Введені паролі не збігаються!';
-        return;
-    } else {
-        label.innerHTML = '';
-    }
-
-    const data = {
-        name: name,
-        surname: surname,
-        email: email,
-        password: password
-    }
-    createNewUser(data);
-}
-
-const createNewUser = (data) => {
-    console.log(data);
-    console.log(data.password);
-    fetch('/auth/register', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            setUserData(data, data.password);
-            removePopUps();
-        })
-        .catch(error => {
-            document.getElementById('registration-label').innerHTML='*Цей email вже зайнято!';
-            console.error('Error:', error);
-        });
-}
-
 const loginUser = () => {
     const label = document.getElementById('login-label');
 
@@ -110,18 +39,16 @@ const loginUser = () => {
     const password = document.getElementById('password').value;
 
     if (email === '' || password === '') {
-        label.innerHTML = '*Заповніть всі поля!';
-        return ;
-    }
-    else {
+        label.innerHTML = '* Заповніть усі поля';
+        return;
+    } else {
         label.innerHTML = '';
     }
     const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if(mailFormat.test(email)) {
+    if (mailFormat.test(email)) {
         label.innerHTML = '';
-    }
-    else {
-        label.innerHTML = '*Цей формат емейлу не відповідає нормам!';
+    } else {
+        label.innerHTML = '* Введіть коректну електронну адресу';
         return false;
     }
     const data = {
@@ -141,32 +68,42 @@ const verifyUser = (input) => {
     })
         .then(response => {
             if (!response.ok) {
-                throw new Error('Network response was not ok');
+                return response.text().then(text => { throw new Error(text) })
             }
             return response.json();
         })
         .then(data => {
-            document.getElementById('login-label').innerHTML='';
+            document.getElementById('login-label').innerHTML = '';
             setUserData(data, input.password);
-            removePopUps();
+            removePopUp();
         })
         .catch(error => {
-            document.getElementById('login-label').innerHTML='*Перевірте введені вами поля!';
-            console.error('Error:', error);
+            let errorBody = JSON.parse(error.message);
+            document.getElementById('login-label').innerHTML = errorParser(errorBody.message);
         });
 
 }
 
 const setUserData = (response, password) => {
     sessionStorage.setItem('id', response.data.id);
-    sessionStorage.setItem('name', response.data.name);
-    sessionStorage.setItem('password', password);
+    sessionStorage.setItem('personal-gas-account', response.data.personalGasAccount);
     sessionStorage.setItem('email', response.data.email);
-    location.reload();
+    window.location.href = '../';
 }
 
-const removePopUps = () => {
+const removePopUp = () => {
     document.querySelector(".login-popup").classList.remove("active");
-
     controlDisapearingBG(0);
+}
+const clearPopUp = () => {
+    document.getElementById('email').value = "";
+    document.getElementById('password').value = "";
+    loginLabel.innerHTML = "";
+}
+const  errorParser = (errorMessage) => {
+    if(errorMessage.includes("password")){
+        return "* Ви ввели невірний пароль"
+    }else{
+        return "* Користувача із введеним логіном не існує"
+    }
 }
