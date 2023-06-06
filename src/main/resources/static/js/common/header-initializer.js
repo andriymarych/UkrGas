20911570b238bb461646dcce924d9402b9e4638d
andriymarych <andriumarich@gmail.com>
2023-06-06 12:34:35 +0300

    Added profile page

--- src/main/resources/static/js/common/header-initializer.js
+++ src/main/resources/static/js/common/header-initializer.js
@@ -106,7 +106,7 @@ async function loadUserPersonalGasAccounts () {
                 'Content-Type': 'application/json',
             }
         };
-        let request =  "/api/v1/personalAccounts/?" + params.toString();
+        let request =  "/api/v1/personal-account/all?" + params.toString();
         const response =
             await fetch(request, settings);
         const responseBody = await response.json();
