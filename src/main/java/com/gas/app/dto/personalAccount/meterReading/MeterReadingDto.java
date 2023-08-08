4f7890a470474fb0ab869a92315f1c89d2654f30
andriymarych <andriumarich@gmail.com>
2023-08-08 18:39:25 +0300

    Delete meter reading DTOs

--- src/main/java/com/gas/app/dto/personalAccount/meterReading/MeterReadingDto.java
+++
@@ -1,6 +0,0 @@
-package com.gas.app.dto.personalAccount.meterReading;
-
-import java.sql.Date;
-
-public record MeterReadingDto(Long id, Double value, Date date, Long personalGasAccountId) {
-}
--- src/main/java/com/gas/app/dto/personalAccount/meterReading/MeterReadingResponseDto.java
+++
@@ -1,8 +0,0 @@
-package com.gas.app.dto.personalAccount.meterReading;
-
-import com.gas.app.entity.personalAccount.MeterReading;
-
-import java.util.List;
-
-public record MeterReadingResponseDto(List<MeterReading> meterReadings) {
-}
